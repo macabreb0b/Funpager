50b64c433f31acf193cab1f166ba4bcc9b8ab7d6
Andrew Richards <drew.rchrds@gmail.com>
2014-03-19 10:38:37 -0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Singlepager::Application.config.secret_key_base = 'b6e285664eac5866e421a7d3dd838c7cd1030bd470769fb9bd6b45dce08c58796f862a17e73f6357cc932d51a459d125014a28165e1d933bb9508a02dc36bf8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Singlepager::Application.config.session_store :cookie_store, key: '_Singlepager_session'
