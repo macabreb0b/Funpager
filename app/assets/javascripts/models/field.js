acc507a24620a77fad3831cc2212ec5e5cf701ab
Andrew <drew.rchrds@gmail.com>
2014-03-21 20:52:13 -0700

    create backbone field subclasses.

--- app/assets/javascripts/models/field.js
+++ app/assets/javascripts/models/field.js
@@ -22,3 +22,62 @@ Singlepager.Models.TextField = Singlepager.Models.Field.extend({
     })
   }
 })
+
+Singlepager.Models.DescriptionField = Singlepager.Models.Field.extend({
+  initialize: function() {
+    this.set({
+      label: 'Description:',
+      content_type: 'textarea',
+      tag: 'p'
+    })
+  }
+})
+
+Singlepager.Models.UrlField = Singlepager.Models.Field.extend({
+  initialize: function() {
+    this.set({
+      label: 'Url:',
+      content_type: 'text',
+      tag: 'p'
+    })
+  }
+})
+
+Singlepager.Models.ImageField = Singlepager.Models.Field.extend({
+  initialize: function() {
+    this.set({
+      label: 'Text:',
+      content_type: 'file',
+      tag: 'img'
+    })
+  }
+})
+
+Singlepager.Models.TelField = Singlepager.Models.Field.extend({
+  initialize: function() {
+    this.set({
+      label: 'Telephone:',
+      content_type: 'tel',
+      tag: 'p'
+    })
+  }
+})
+
+Singlepager.Models.EmailField = Singlepager.Models.Field.extend({
+  initialize: function() {
+    this.set({
+      label: 'Email:',
+      content_type: 'email',
+      tag: 'a'
+    })
+  }
+})
+
+Singlepager.Models.AddressAll = Singlepager.Models.Field.extend({
+  initialize: function() {
+    this.set({
+      content_type: 'text',
+      tag: 'p'
+    })
+  }
+})
